e28cc99e2ce4c363c74295c5e3ea7b510bb8935b
khushi89012 <khushi1399gupta@gmail.com>
2025-05-16 00:07:00 +0530

    test: add returns number when input is a single number

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -4,4 +4,8 @@ describe("String Calculator", () => {
   test("returns 0 for an empty string", () => {
     expect(add("")).toBe(0);
   });
+
+  test("returns number itself for single number", () => {
+    expect(add("1")).toBe(1);
+  });
 });
