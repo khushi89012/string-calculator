e9b815f0105f19b01804221f7a09b2263bd8f797
khushi89012 <khushi1399gupta@gmail.com>
2025-05-16 00:11:44 +0530

    feat: throw exception for single negative number

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -27,5 +27,9 @@ describe("String Calculator", () => {
   test("supports custom delimiter |", () => {
     expect(add("//|\n1|2|3")).toBe(6);
   });
+  test("throws error for a single negative number", () => {
+    expect(() => add("1,-2")).toThrow("negative numbers not allowed -2");
+  });
+
 
 });
