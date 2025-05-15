8fd77b455e188ec925dc2e379104909d438b2346
khushi89012 <khushi1399gupta@gmail.com>
2025-05-16 00:10:45 +0530

    test: add support for custom delimiters in add function

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -16,4 +16,16 @@ describe("String Calculator", () => {
     test("returns sum of multiple comma-separated numbers", () => {
     expect(add("1,2,3,4")).toBe(10);
   });
+  test("supports custom delimiter ;", () => {
+    expect(add("//;\n1;2")).toBe(3);
+  });
+
+  test("supports custom delimiter *", () => {
+    expect(add("//*\n2*3*4")).toBe(9);
+  });
+
+  test("supports custom delimiter |", () => {
+    expect(add("//|\n1|2|3")).toBe(6);
+  });
+
 });
