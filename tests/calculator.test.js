3eb0d9f8f2e5b5bb119a57e6c9870939e0a28d41
khushi89012 <khushi1399gupta@gmail.com>
2025-05-16 00:09:08 +0530

    test: add returns sum of multiple comma-separated numbers

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -12,4 +12,8 @@ describe("String Calculator", () => {
   test("returns sum of two numbers", () => {
     expect(add("1,2")).toBe(3);
   });
+
+    test("returns sum of multiple comma-separated numbers", () => {
+    expect(add("1,2,3,4")).toBe(10);
+  });
 });
